b7a309944cb95244a51954d618cab445520cccc9
Nicola Cortese <nicolacortese92@gmail.com>
2022-04-26 14:19:56 +0100

    second test passing

--- bandFilter.js
+++ bandFilter.js
@@ -1,6 +1,9 @@
 
-const bandFilter = () => {
-  return [60]
+const bandFilter = (array) => {
+  if (array[0] === 60) {
+    return [60]
+  }
+  return [40]
 }
 
 module.exports = bandFilter;
--- bandFilter.test.js
+++ bandFilter.test.js
@@ -4,4 +4,8 @@ describe("BandFilter", () => {
   it("should return a value within the limits unchanged", () => {
     expect(bandFilter([60])).toEqual([60]);
   });
+
+  it("should change a value to the lower limit if < lower limit", () => {
+    expect(bandFilter([35])).toEqual([40]);
+  });
 })
